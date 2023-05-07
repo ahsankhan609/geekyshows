2e2371bc65d634924f387bcce39c316baf8f3712
ahsankhan609 <ahsankhan609@gmail.com>
2023-05-07 11:55:39 +0500

    Update Environemnt

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\IT LAND\Desktop\django-projects\geekyshows\.venv
+set VIRTUAL_ENV=C:\Users\AhsanKhan\Desktop\django-projects\geekyshows\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
