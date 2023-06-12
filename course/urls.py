9ac81c918d93de055af95e22a942155799bb4e9b
ahsankhan609 <ahsankhan609@gmail.com>
2023-06-12 21:01:54 +0500

    Updated Cookie URLS

--- course/urls.py
+++ course/urls.py
@@ -3,9 +3,12 @@ from course.views import *
 
 urlpatterns = [
     path('',home),
-    path('python',python),
+    path('python/',python),
     path('all/',courses),
     path('html_detail/',html_detail),
     path('css_detail/',css_detail,name='css_detail'),
+    path('set/',setcookie,name='set-cookie'),
+    path('get/',getcookie,name='get-cookie'),
+    path('del/',delcookie,name='del-cookie'),
 ]
 
