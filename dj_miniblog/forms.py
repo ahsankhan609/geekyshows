882709ced27027d3a1d60be6d29fb97d2f6b48fe
ahsankhan609 <ahsankhan609@gmail.com>
2023-06-13 22:19:53 +0500

    update todo tag

--- dj_miniblog/forms.py
+++ dj_miniblog/forms.py
@@ -59,3 +59,5 @@ class PostForm (forms .ModelForm) :
         labels = {'title': 'Title','description': 'Description'}
         widgets = {'title':forms.TextInput(attrs={'class': 'form-control'}),
         'description':forms.Textarea(attrs={'class': 'form-control'}), }
+        
+//TODO: add me later
